36d7bfdde41e9e3d903ce43dca1fff80891d72c7
Dimitris Kalligas <di.kalligas@gmail.com>
2026-03-11 13:51:32 +0200

    removed AGGrid page for manufacturers

--- app/manufacturer/pageAGGrid.tsx
+++
@@ -1,7 +0,0 @@
-import ManufacturerTable from '@/components/ManufacturerTable';
-import shadCNtable from '@/app/manufacturer/page';
-
-export default function ManufacturersPage() {
-  // return <ManufacturerTable />; // AG-Grid version
-  return <shadCNtable />; // shadcn version 
-}
